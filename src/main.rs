bfc714bd52950b417735d6e69a87368df226543e
Zanciks <Zackarysemancik@gmail.com>
2024-01-26 21:50:46 +0000

    restarting implimentation of everything again lolol

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-use chess::{self, Board};
-
-mod uci;
-mod calculate;
-mod transposition_table;
+use chess:*;
 
 fn main() {
-
-    run_engine();
-}
-
-fn run_engine() {
-    let mut board = Board::default();
-    loop {
-        uci::commands(&mut board);
-    }
+    let board = chess::Board::default();
 }
