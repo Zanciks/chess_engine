d4e28a5283c1daeca7737a9f0201aead7bbb7b83
Zanciks <Zackarysemancik@gmail.com>
2024-01-27 00:25:39 +0000

    technically functioning minimax function

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,33 +1,33 @@
 #![allow(dead_code, unused_variables)]
 
-use chess::{Board, BoardStatus, ChessMove, MoveGen};
-
+use chess::{Board, BoardStatus, ChessMove, Color, MoveGen};
 use crate::evaluation::evaluate;
 
-pub fn calculate(board: Board) -> (i32, ChessMove) {
-    let mut best_eval = i32::MIN;
+pub fn find_best_move(board: Board, depth: u8) -> (i32, ChessMove) {
+    let maximizing = if board.side_to_move() == Color::White {true} else {false};
+    let (best_eval, best_move) = minimax(board, depth, maximizing);
+
+    return (best_eval, best_move);
+}
+
+pub fn minimax(board: Board, depth: u8, maximizing: bool) -> (i32, ChessMove) {
+    if board.status() == BoardStatus::Checkmate {return (-i32::MAX, ChessMove::default())}
+    if depth == 0 {return (evaluate(board), ChessMove::default())}
+
     let mut best_move = ChessMove::default();
+    let mut best_eval: i32;
+    if maximizing {best_eval = -i32::MAX} else {best_eval = i32::MAX}
+
+    let color_factor = if board.side_to_move() == chess::Color::White { 1 } else { -1 };
 
     for mv in MoveGen::new_legal(&board) {
         let new_board = board.make_move_new(mv);
-        let eval = -minimax(new_board, 2, best_eval);
-        if eval > best_eval {
+        let eval = minimax(new_board, depth - 1, !maximizing).0;
+        if eval * color_factor > best_eval * color_factor {
             best_eval = eval;
             best_move = mv;
         }
     }
-    return (best_eval, best_move);
-}
 
-pub fn minimax(board: Board, depth: u8, mut best_eval: i32) -> i32 {
-    if board.status() == BoardStatus::Checkmate {return -i32::MAX } // we can negate MAX, but we CANT negate MIN, so we just use -MAX lol
-    if depth == 0 {return evaluate(board)}
-
-    for mv in MoveGen::new_legal(&board) {
-        let new_board = board.make_move_new(mv);
-        let eval = -minimax(new_board, depth - 1, best_eval);
-        if eval > best_eval {best_eval = eval}
-    }
-
-    return best_eval;
+    return (best_eval, best_move)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@ mod calculation;
 
 use std::str::FromStr;
 
-use calculation::calculate;
 use chess::*;
 
 fn main() {
-    let board = Board::from_str("rnbqkbnr/pppp1ppp/4p3/8/6P1/5P2/PPPPP2P/RNBQKBNR b KQkq - 0 1").unwrap();
-    let (eval, mv) = calculate(board);
+    let board = Board::from_str("r4rk1/pp1n2p1/2pb2qp/3p1p2/3P2bN/1NPBB3/PPQ2PK1/R6R b - - 5 17").unwrap();
+    let (eval, mv) = calculation::find_best_move(board, 3);
     println!("{}, {}", eval, mv); 
 }
