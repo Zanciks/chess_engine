23540c019f73b5339150a35915b925106a4ee1f2
Zanciks <Zackarysemancik@gmail.com>
2024-01-27 00:57:01 +0000

    removed maximizing: bool

--- src/calculation.rs
+++ src/calculation.rs
@@ -5,24 +5,24 @@ use crate::evaluation::evaluate;
 
 pub fn find_best_move(board: Board, depth: u8) -> (i32, ChessMove) {
     let maximizing = if board.side_to_move() == Color::White {true} else {false};
-    let (best_eval, best_move) = minimax(board, depth, maximizing);
+    let (best_eval, best_move) = minimax(board, depth);
 
     return (best_eval, best_move);
 }
 
-pub fn minimax(board: Board, depth: u8, maximizing: bool) -> (i32, ChessMove) {
+pub fn minimax(board: Board, depth: u8) -> (i32, ChessMove) {
     if board.status() == BoardStatus::Checkmate {return (-i32::MAX, ChessMove::default())}
     if depth == 0 {return (evaluate(board), ChessMove::default())}
 
     let mut best_move = ChessMove::default();
     let mut best_eval: i32;
-    if maximizing {best_eval = -i32::MAX} else {best_eval = i32::MAX}
+    if board.side_to_move() == Color::White {best_eval = -i32::MAX} else {best_eval = i32::MAX}
 
-    let color_factor = if board.side_to_move() == chess::Color::White { 1 } else { -1 };
+    let color_factor = if board.side_to_move() == Color::White { 1 } else { -1 };
 
     for mv in MoveGen::new_legal(&board) {
         let new_board = board.make_move_new(mv);
-        let eval = minimax(new_board, depth - 1, !maximizing).0;
+        let eval = minimax(new_board, depth - 1).0;
         if eval * color_factor > best_eval * color_factor {
             best_eval = eval;
             best_move = mv;
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::str::FromStr;
 use chess::*;
 
 fn main() {
-    let board = Board::from_str("r4rk1/pp1n2p1/2pb2qp/3p1p2/3P2bN/1NPBB3/PPQ2PK1/R6R b - - 5 17").unwrap();
+    let board = Board::from_str("3r1knr/pP3ppp/5q2/2b5/Q3p3/8/PPP2PPP/RNB2RK1 b - - 2 15").unwrap();
     let (eval, mv) = calculation::find_best_move(board, 3);
-    println!("{}, {}", eval, mv); 
+    println!("{}, {}", eval, mv);
 }
