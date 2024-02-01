6bc154d490d78a55afaebdd4af2426e907bb2413
Zanciks <Zackarysemancik@gmail.com>
2024-02-01 22:16:01 +0000

    need to play with eval more. want to rewrite my own function

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,6 +1,15 @@
-use chess::{Board, Color, Piece, ALL_SQUARES};
+use std::cmp::max;
+
+use chess::{Board, Color, Piece, Square, ALL_SQUARES};
 
 pub fn evaluate(board: Board) -> i32 {
+    let endgame_weight = endgame_weight(board);
+    let mut eval = count_material(board);
+    eval -= force_king_to_corner_endgame(board.king_square(board.side_to_move()), board.king_square(!board.side_to_move()), endgame_weight);
+    return eval;
+}
+
+fn count_material(board: Board) -> i32 {
     let mut eval: i32 = 0;
     for square in ALL_SQUARES {
         if let (Some(piece), Some(color)) = (board.piece_on(square), board.color_on(square)) {
@@ -23,4 +32,41 @@ pub fn evaluate(board: Board) -> i32 {
     }
     if board.side_to_move() == Color::Black {eval *= -1}
     eval
+}
+
+fn endgame_weight(board: Board) -> i32 {
+    let pawns = board.pieces(Piece::Pawn).popcnt();
+    let knights = board.pieces(Piece::Knight).popcnt();
+    let bishops = board.pieces(Piece::Bishop).popcnt();
+    let rooks = board.pieces(Piece::Rook).popcnt();
+    let queens = board.pieces(Piece::Queen).popcnt();
+
+    let count = (pawns + knights + bishops + rooks + queens) as f32;
+
+    100 - (count * 10.0 / 3.0) as i32
+}
+
+fn force_king_to_corner_endgame(friendly_king: Square, enemy_king: Square, endgame_weight: i32) -> i32 {
+    // very much inspired (if not just stolen!) from https://youtu.be/U4ogK0MIzqk?si=7ONAEMHrscMBqbwW&t=1063
+    let mut evaluation = 0;
+
+    let enemy_file = enemy_king.get_file().to_index() as i32;
+    let enemy_rank = enemy_king.get_rank().to_index() as i32;
+
+    let distance_to_center_file = max(3 - enemy_file, enemy_file - 4);
+    let distance_to_center_rank = max(3 - enemy_rank, enemy_rank - 4);
+
+    let distance_to_center = distance_to_center_file + distance_to_center_rank;
+    evaluation += distance_to_center;
+
+    let friendly_file = friendly_king.get_file().to_index() as i32;
+    let friendly_rank = friendly_king.get_rank().to_index() as i32;
+
+    let file_dist = (friendly_file - enemy_file).abs();
+    let rank_dist = (friendly_rank - enemy_rank).abs();
+
+    let dist = file_dist + rank_dist;
+    evaluation += 14 - dist;
+
+    evaluation * endgame_weight
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::str::FromStr;
 use chess::*;
 
 fn main() {
-    let board = Board::from_str("r2q1r1k/1p2Np1p/p1pp2n1/5bQ1/7R/5P2/PPP3PP/R5K1 w - - 0 23").unwrap();
-    let (eval, mv) = calculation::find_best_move(board, 3);
+    let board = Board::from_str("3r4/8/2k5/8/2K5/8/4r3/8 b - - 6 4").unwrap();
+    let (eval, mv) = calculation::find_best_move(board, 5);
     println!("{}, {}", eval, mv);
 }
