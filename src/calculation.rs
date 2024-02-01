215e775571b88ee0c402755455c0342212ea8738
Zanciks <Zackarysemancik@gmail.com>
2024-02-01 21:07:16 +0000

    working minimax

--- src/calculation.rs
+++ src/calculation.rs
@@ -5,29 +5,40 @@ use crate::evaluation::evaluate;
 
 pub fn find_best_move(board: Board, depth: u8) -> (i32, ChessMove) {
     let maximizing = if board.side_to_move() == Color::White {true} else {false};
-    let (best_eval, best_move) = minimax(board, depth);
 
-    return (best_eval, best_move);
+
+    return minimax(board, depth);
 }
 
 pub fn minimax(board: Board, depth: u8) -> (i32, ChessMove) {
-    if board.status() == BoardStatus::Checkmate {return (-i32::MAX, ChessMove::default())}
+    if board.status() == BoardStatus::Checkmate {
+        if board.side_to_move() == Color::White {return (-i32::MAX, ChessMove::default())}
+        else {return (i32::MAX, ChessMove::default())}
+    }
+
     if depth == 0 {return (evaluate(board), ChessMove::default())}
 
+    let mut best_eval = if board.side_to_move() == Color::White {-i32::MAX} else {i32::MAX};
     let mut best_move = ChessMove::default();
-    let mut best_eval: i32;
-    if board.side_to_move() == Color::White {best_eval = -i32::MAX} else {best_eval = i32::MAX}
-
-    let color_factor = if board.side_to_move() == Color::White { 1 } else { -1 };
 
     for mv in MoveGen::new_legal(&board) {
         let new_board = board.make_move_new(mv);
         let eval = minimax(new_board, depth - 1).0;
-        if eval * color_factor > best_eval * color_factor {
-            best_eval = eval;
-            best_move = mv;
+        match board.side_to_move() {
+            Color::White => {
+                if eval > best_eval {
+                    best_eval = eval;
+                    best_move = mv;
+                }
+            },
+            Color::Black => {
+                if eval < best_eval {
+                    best_eval = eval;
+                    best_move = mv;
+                }
+            }
         }
     }
-
+    
     return (best_eval, best_move)
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::str::FromStr;
 use chess::*;
 
 fn main() {
-    let board = Board::from_str("3r1knr/pP3ppp/5q2/2b5/Q3p3/8/PPP2PPP/RNB2RK1 b - - 2 15").unwrap();
+    let board = Board::from_str("4R3/pp1r2kp/1q3pp1/8/2Q5/7P/P4PP1/5K2 w - - 0 29").unwrap();
     let (eval, mv) = calculation::find_best_move(board, 3);
     println!("{}, {}", eval, mv);
 }
