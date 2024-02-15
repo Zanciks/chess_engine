559409e5bf57642ecbc07472e6ddfe2462092ceb
Zanciks <Zackarysemancik@gmail.com>
2024-02-15 22:32:53 +0000

    added gamephase weights and some todo/to-use code

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,12 +1,16 @@
 use chess::{Board, Color, Piece, ALL_SQUARES};
 
 pub fn evaluate(board: Board) -> i32 {
-    let mut eval = count_pieces(board);
+    let (_opening_weight, _middlegame_weight, _endgame_weight) = game_phase_weights(board);
+
+    let mut eval = count_material(board);
+    // eval += endgame_push_king_to_corner(board, endgame_weight);
+
     if board.side_to_move() == Color::Black {eval *= -1}
     eval
 }
 
-pub fn count_pieces(board: Board) -> i32 {
+pub fn count_material(board: Board) -> i32 {
     let mut sum = 0;
 
     for square in ALL_SQUARES {
@@ -31,5 +35,30 @@ pub fn count_pieces(board: Board) -> i32 {
         }
     }
 
+    sum
+}
+
+fn _endgame_push_king_to_corner(_board: Board, _endgame_weight: i32) -> i32 {
+    todo!()
+}
+
+// we're going to take in the board and return 3 values from 0 to 100 representing opening, middlegame, endgame
+// currently this function won't deal with openings because it's kinda complicated tbh
+fn game_phase_weights(board: Board) -> (i32, i32, i32) {
+    let piece_count = count_pieces(board);
+
+    let middlegame_weight = piece_count * 100 / 32;
+    let endgame_weight = 100 - middlegame_weight;
+
+    (0, middlegame_weight, endgame_weight)
+}
+
+fn count_pieces(board: Board) -> i32 {
+    let mut sum = 0;
+    for square in ALL_SQUARES {
+        if let Some(_) = board.piece_on(square) {
+            sum += 1
+        }
+    }
     sum
 }
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ mod calculation;
 mod tests;
 
 fn main() {
-    let board = chess::Board::from_str("3q3k/8/8/8/8/8/8/3Q3K b - - 0 1").unwrap();
-    let (eval, mv) = calculation::find_best_move(board, 1);
+    let board = chess::Board::from_str("7r/8/3k4/8/5K2/8/1r6/8 b - - 12 7").unwrap();
+    let (eval, mv) = calculation::find_best_move(board, 5);
     println!("{} | {}", eval, mv);
+    evaluation::evaluate(board);
 }
