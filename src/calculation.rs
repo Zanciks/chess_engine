2ad519f3bb98cbe607c52bf0c6737f418d41cef2
Zanciks <Zackarysemancik@gmail.com>
2024-02-15 21:47:23 +0000

    added alpha beta pruning

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,14 +1,16 @@
+use std::cmp::max;
+
 use chess::{Board, BoardStatus, ChessMove, Color, MoveGen};
 use crate::evaluation::evaluate;
 
 pub fn find_best_move(board: Board, depth: u8) -> (i32, ChessMove) {
-    let (mut eval, best_move) = negamax(board, depth);
+    let (mut eval, best_move) = negamax(board, depth, -i32::MAX, i32::MAX);
     if board.side_to_move() == Color::Black {eval *= -1}
 
     return (eval, best_move)
 }
 
-pub fn negamax(board: Board, depth: u8) -> (i32, ChessMove) {
+pub fn negamax(board: Board, depth: u8, mut alpha: i32, beta: i32) -> (i32, ChessMove) {
     if board.status() == BoardStatus::Checkmate {return (-i32::MAX, ChessMove::default())}
 
     if depth == 0 {return (evaluate(board), ChessMove::default())}
@@ -18,11 +20,16 @@ pub fn negamax(board: Board, depth: u8) -> (i32, ChessMove) {
 
     for mv in MoveGen::new_legal(&board) {
         let new_board = board.make_move_new(mv);
-        let eval = -negamax(new_board, depth - 1).0;
+        let eval = -negamax(new_board, depth - 1, -beta, -alpha).0;
+        
         if eval > best_eval {
             best_eval = eval;
             best_move = mv;
         }
+        
+        alpha = max(alpha, best_eval);
+        if alpha >= beta {break}
+
     }
     return (best_eval, best_move)
 }
