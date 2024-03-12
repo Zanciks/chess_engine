bc4ede026f81addb267727d75e08ac10241d719e
Zanciks <Zackarysemancik@gmail.com>
2024-03-12 21:30:26 +0000

    sort moves before calculating

--- src/calculation.rs
+++ src/calculation.rs
@@ -1,6 +1,6 @@
 use std::cmp::max;
 
-use chess::{Board, BoardStatus, ChessMove, Color, MoveGen};
+use chess::{Board, BoardStatus, ChessMove, Color, MoveGen, Piece};
 use crate::evaluation::evaluate;
 
 pub fn find_best_move(board: Board, depth: u8) -> (i32, ChessMove) {
@@ -18,7 +18,7 @@ fn negamax(board: Board, depth: u8, mut alpha: i32, beta: i32) -> (i32, ChessMov
     let mut best_eval = -i32::MAX;
     let mut best_move = ChessMove::default();
 
-    for mv in MoveGen::new_legal(&board) {
+    for mv in sort_moves(&board) {
         let new_board = board.make_move_new(mv);
         let eval = -negamax(new_board, depth - 1, -beta, -alpha).0;
         
@@ -32,4 +32,33 @@ fn negamax(board: Board, depth: u8, mut alpha: i32, beta: i32) -> (i32, ChessMov
     }
 
     (best_eval, best_move)
+}
+
+pub fn sort_moves(board: &Board) -> Vec<ChessMove> {
+    let mut vector: Vec<(ChessMove, i32)> = vec![];
+    for mv in MoveGen::new_legal(board) {
+        if let Some(dest_piece) = board.piece_on(mv.get_dest()) {
+            let dest_value = piece_to_i32(dest_piece);
+            vector.push((mv, dest_value));
+        } else {
+            vector.push((mv, 0));
+        }
+    }
+
+    vector.sort_by_key(|item| item.1);
+    vector.reverse();
+
+    let output = vector.into_iter().map(|item| item.0).collect();
+    output
+}
+
+pub fn piece_to_i32(piece: Piece) -> i32 {
+    match piece {
+        Piece::Pawn => 100,
+        Piece::Knight => 300,
+        Piece::Bishop => 300,
+        Piece::Rook => 500,
+        Piece::Queen => 900,
+        _ => 0
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::str::FromStr;
 
 mod evaluation;
 mod calculation;
-mod benchmarks;
+mod tests;
 
 fn main() {
     let board = chess::Board::from_str("4rBk1/2N2pPp/1p6/1Knb1Nb1/r5P1/8/1P6/6RR b - - 9 37").unwrap();
