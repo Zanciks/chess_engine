d23434403b364a7b1c62aaaa8e797358e4daa8f7
Zanciks <Zackarysemancik@gmail.com>
2024-03-12 19:54:12 +0000

    Refactored tests to benchmarks

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::str::FromStr;
 
 mod evaluation;
 mod calculation;
-mod tests;
+mod benchmarks;
 
 fn main() {
-    let board = chess::Board::from_str("7k/8/8/8/8/8/8/R6K w - - 0 1").unwrap();
-    let (eval, mv) = calculation::find_best_move(board, 5);
+    let board = chess::Board::from_str("4rBk1/2N2pPp/1p6/1Knb1Nb1/r5P1/8/1P6/6RR b - - 9 37").unwrap();
+    let (eval, mv) = calculation::find_best_move(board, 7);
     println!("{} | {}", eval, mv);
 }
