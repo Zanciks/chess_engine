856458a8d8083b5ad2cd53cc64fcc193f83dd65e
Zanciks <Zackarysemancik@gmail.com>
2024-10-04 19:30:22 +0000

    using shakmaty

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,64 +1,23 @@
-use chess::{Board, Color, Piece, ALL_SQUARES};
+use shakmaty::{Square, board::Board, Color, Role};
 
-pub fn evaluate(board: Board) -> i32 {
-    let (_opening_weight, _middlegame_weight, _endgame_weight) = game_phase_weights(board);
-
-    let mut eval = count_material(board);
-    // eval += endgame_push_king_to_corner(board, endgame_weight);
-
-    if board.side_to_move() == Color::Black {eval *= -1}
-    eval
-}
-
-pub fn count_material(board: Board) -> i32 {
-    let mut sum = 0;
-
-    for square in ALL_SQUARES {
-        if let Some(piece) = board.piece_on(square) {
-            if let Some(color) = board.color_on(square) {
-                sum += match (piece, color) {
-                    (Piece::Pawn,   Color::White) =>  100,
-                    (Piece::Knight, Color::White) =>  300,
-                    (Piece::Bishop, Color::White) =>  300,
-                    (Piece::Rook,   Color::White) =>  500,
-                    (Piece::Queen,  Color::White) =>  500,
-
-                    (Piece::Pawn,   Color::Black) => -100,
-                    (Piece::Knight, Color::Black) => -300,
-                    (Piece::Bishop, Color::Black) => -300,
-                    (Piece::Rook,   Color::Black) => -500,
-                    (Piece::Queen,  Color::Black) => -900,
-
-                    _ => 0
-                }
+pub fn evaluate(board: &Board) -> i32 {
+    let mut evaluation = 0;
+    for square in Square::ALL {
+        if let Some(piece) = board.piece_at(square) {
+            evaluation += match (piece.color, piece.role) {
+                (Color::White, Role::Pawn)   => 100,
+                (Color::White, Role::Knight) => 300,
+                (Color::White, Role::Bishop) => 300,
+                (Color::White, Role::Rook)   => 500,
+                (Color::White, Role::Queen)  => 900,
+                (Color::Black, Role::Pawn)   => -100,
+                (Color::Black, Role::Knight) => -300,
+                (Color::Black, Role::Bishop) => -300,
+                (Color::Black, Role::Rook)   => -500,
+                (Color::Black, Role::Queen)  => -900,
+                _ => 0,
             }
         }
     }
-
-    sum
-}
-
-fn _endgame_push_king_to_corner(_board: Board, _endgame_weight: i32) -> i32 {
-    todo!()
-}
-
-// we're going to take in the board and return 3 values from 0 to 100 representing opening, middlegame, endgame
-// currently this function won't deal with openings because it's kinda complicated tbh
-fn game_phase_weights(board: Board) -> (i32, i32, i32) {
-    let piece_count = count_pieces(board);
-
-    let middlegame_weight = piece_count * 100 / 32;
-    let endgame_weight = 100 - middlegame_weight;
-
-    (0, middlegame_weight, endgame_weight)
-}
-
-fn count_pieces(board: Board) -> i32 {
-    let mut sum = 0;
-    for square in ALL_SQUARES {
-        if let Some(_) = board.piece_on(square) {
-            sum += 1
-        }
-    }
-    sum
+    return evaluation;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-use std::str::FromStr;
-
+use shakmaty::{Chess, Position};
 mod evaluation;
-mod calculation;
-mod tests;
 
 fn main() {
-    let board = chess::Board::from_str("4rBk1/2N2pPp/1p6/1Knb1Nb1/r5P1/8/1P6/6RR b - - 9 37").unwrap();
-    let (eval, mv) = calculation::find_best_move(board, 7);
-    println!("{} | {}", eval, mv);
+    let pos = Chess::default();
+    println!("{}", evaluation::evaluate(pos.board()));
 }
